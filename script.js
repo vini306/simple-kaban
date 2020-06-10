362663cf88bf19fe71b24e17be6a2dc28c1a6834
vinicius dos santos mota da silva <viniciussantos306@gmail.com>
2020-06-10 13:06:37 -0300

    -add drag'n drop

--- script.js
+++ script.js
@@ -1,46 +1,45 @@
-const cards = document.querySelectorAll('.card')
-const dropzones = document.querySelectorAll('.dropzone')
+const cards = document.querySelectorAll(".card");
+const dropzones = document.querySelectorAll(".dropzone");
 
-cards.forEach(card => {
-  card.addEventListener('dragstart', dragstart)
-  card.addEventListener('drag', drag)
-  card.addEventListener('dragend', dragend)
-})
-
+cards.forEach((card) => {
+  card.addEventListener("dragstart", dragstart);
+  card.addEventListener("drag", drag);
+  card.addEventListener("dragend", dragend);
+});
 
 function dragstart() {
-  console.log()
+  dropzones.forEach((dropzone) => dropzone.classList.add("highlight"));
+  this.classList.add("is-dragging");
 }
 
-function drag() {
-
-}
+function drag() {}
 
 function dragend() {
-
+  dropzones.forEach((dropzone) => dropzone.classList.remove("highlight"));
+  this.classList.remove("is-dragging");
 }
 
-
-dropzones.forEach(dropzone => {
-  dropzone.addEventListener('dragenter', dragenter)
-  dropzone.addEventListener('dragover ', dragover)
-  dropzone.addEventListener('dragleave', dragleave)
-  dropzone.addEventListener('drop', drop)
-})
-
+dropzones.forEach((dropzone) => {
+  dropzone.addEventListener("dragenter", dragenter);
+  dropzone.addEventListener("dragover", dragover);
+  dropzone.addEventListener("dragleave", dragleave);
+  dropzone.addEventListener("drop", drop);
+});
 
 function dragenter() {
-  console.log()
+  console.log();
 }
 
 function dragover() {
+  this.classList.add("over");
 
+  const cardDragged = document.querySelector(".is-dragging");
+
+  this.appendChild(cardDragged);
 }
 
 function dragleave() {
-
+  this.classList.remove("over");
 }
 
-function drop() {
-
-}
+function drop() {}
